f5a9a29cae3e7b1954f9001a0570cd4da86091b7
coolga <coolgadv@gmail.com>
2016-09-09 07:49:45 +0300

    Frontend: jQuery import

--- frontend/src/main/resources/gulpfile.js
+++ frontend/src/main/resources/gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 
-gulp.task('copy', function() {
+gulp.task('copy-js-lib', function() {
+    gulp.src(['node_modules/jquery/dist/jquery.js'])
+        .pipe(gulp.dest('dist/js/lib'));
+});
+
+gulp.task('copy-src', function() {
     gulp.src('src/*')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['copy']);
+gulp.task('default', ['copy-src', 'copy-js-lib']);
