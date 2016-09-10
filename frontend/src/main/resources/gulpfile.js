517c381415974e5fdcd1cdb2142c9d80b75d7845
coolga <coolgadv@gmail.com>
2016-09-10 09:10:32 +0300

    Frontend: build simplification + migration to ES6 for Gulp build script

--- frontend/src/main/resources/gulpfile.js
+++ frontend/src/main/resources/gulpfile.js
@@ -1,33 +1,29 @@
 const gulp = require('gulp');
 const typings = require('gulp-typings');
-const del = require('del');
 const ts = require('gulp-typescript');
 
-gulp.task('clean', function(cb) {
-    del.sync('dist');
-    cb();
-});
+const dist = '../../../target/dist';
 
-gulp.task('typings', function() {
-    return gulp.src('typings.json')
-        .pipe(typings());
-});
+gulp.task('typings', () =>
+    gulp.src('typings.json')
+        .pipe(typings())
+);
 
-gulp.task('ts', ['typings', 'clean'], function() {
-    return gulp.src(['src/ts/*.ts', 'typings/index.d.ts'])
+gulp.task('ts', ['typings'], () =>
+    gulp.src(['src/ts/*.ts', 'typings/index.d.ts'])
         .pipe(ts(ts.createProject('tsconfig.json')))
         .js
-        .pipe(gulp.dest('dist/js'));
-});
+        .pipe(gulp.dest(dist + '/js'))
+);
 
-gulp.task('copy-js-lib', ['clean'], function() {
-    return gulp.src(['node_modules/jquery/dist/jquery.js'])
-        .pipe(gulp.dest('dist/js/lib'));
-});
+gulp.task('copy-js-lib', () =>
+    gulp.src(['node_modules/jquery/dist/jquery.js'])
+        .pipe(gulp.dest(dist + '/js/lib'))
+);
 
-gulp.task('copy-src', ['clean'], function() {
-    return gulp.src(['src/*', '!src/ts'])
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('copy-src', () =>
+    gulp.src(['src/*', '!src/ts'])
+        .pipe(gulp.dest(dist))
+);
 
 gulp.task('default', ['copy-src', 'ts', 'copy-js-lib']);
