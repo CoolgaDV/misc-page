9454eceadf047cc9543181b4093cc21d33ed3304
coolga <coolgadv@gmail.com>
2016-09-12 07:55:10 +0300

    Frontend: css fix

--- frontend/src/main/resources/src/ts/index.ts
+++ frontend/src/main/resources/src/ts/index.ts
@@ -2,8 +2,8 @@
  *
  */
 $(() => {
-    initApplication($('#123'), new UuidApplication());
-    initApplication($('#123'), new Base64Application());
+    initApplication($('#uuid-shortcut'), new UuidApplication());
+    initApplication($('#base64-shortcut'), new Base64Application());
 });
 
 function initApplication(widget: JQuery, application: Application): void {
