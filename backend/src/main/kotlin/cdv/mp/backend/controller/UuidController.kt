8ebc397ac2846c8d583826e550cfcecaf46ced2e
coolga <coolgadv@gmail.com>
2016-10-20 07:35:11 +0300

    Uuid controller + disable console log output for integration tests

--- backend/src/main/kotlin/cdv/mp/backend/controller/UuidController.kt
+++ backend/src/main/kotlin/cdv/mp/backend/controller/UuidController.kt
@@ -1,6 +1,9 @@
 package cdv.mp.backend.controller
 
 import cdv.mp.backend.service.UuidService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -10,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController
  * *         12.10.2016 07:10
  */
 @RestController
-class UuidController(private val service: UuidService)
+@RequestMapping("/uuid")
+class UuidController(private val service: UuidService) {
+
+    @RequestMapping(value = "/generate", method = arrayOf(RequestMethod.GET))
+    fun generateUuid(@RequestParam("braces") braces: Boolean,
+                     @RequestParam("hyphens") hyphens: Boolean,
+                     @RequestParam("uppercase") uppercase: Boolean): String {
+        return service.generateUuid(braces = braces, hyphens = hyphens, uppercase = uppercase)
+    }
+
+}
--- backend/src/test/kotlin/cdv/mp/backend/IntegrationTest.kt
+++ backend/src/test/kotlin/cdv/mp/backend/IntegrationTest.kt
@@ -1,14 +1,17 @@
 package cdv.mp.backend
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.IntegrationTest
 import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
 
 /**
  * Set of integration tests for REST API
@@ -20,6 +23,7 @@ import org.springframework.web.client.RestTemplate
 @SpringApplicationConfiguration(classes = arrayOf(Bootstrap::class))
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
+@TestPropertySource(properties = arrayOf("spring.main.banner-mode=off"))
 class IntegrationTest {
 
     /** Injects port number of application  */
@@ -36,6 +40,15 @@ class IntegrationTest {
     @Test
     fun testEncode() {
         assertEquals("Zm9v", template.postForEntity(url("base64/encode"), "foo", String::class.java).body)
+    }
+
+    @Test
+    fun testGenerateUuid() {
+        val query = UriComponentsBuilder.fromHttpUrl(url("uuid/generate"))
+                .queryParam("braces", false.toString())
+                .queryParam("hyphens", false.toString())
+                .queryParam("uppercase", false.toString())
+        assertNotNull(template.getForEntity(query.toUriString(), String::class.java).body)
     }
 
     private fun url(postfix: String) = "http://localhost:$port/$postfix"
