f098888d98946146377a3968bd91ee3fb4acd745
coolga <coolgadv@gmail.com>
2016-11-12 11:13:05 +0300

    Backend: management port and pid file writer

--- backend/src/main/kotlin/cdv/mp/backend/Bootstrap.kt
+++ backend/src/main/kotlin/cdv/mp/backend/Bootstrap.kt
@@ -3,6 +3,7 @@ package cdv.mp.backend
 import cdv.mp.backend.config.SpringApplicationConfiguration
 import cdv.mp.backend.config.SpringWebConfiguration
 import org.springframework.boot.Banner
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Import
@@ -18,5 +19,8 @@ import org.springframework.context.annotation.Import
 class Bootstrap
 
 fun main(args: Array<String>) {
-    SpringApplicationBuilder(Bootstrap::class.java).bannerMode(Banner.Mode.OFF).run(*args)
+    SpringApplicationBuilder(Bootstrap::class.java)
+            .listeners(ApplicationPidFileWriter())
+            .bannerMode(Banner.Mode.OFF)
+            .run(*args)
 }
